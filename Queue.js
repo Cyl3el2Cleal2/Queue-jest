2a76f8da79fac27c4c169c2b8903cd6b087c9ceb
Cyl3el2Cleal2 <natha_sk@hotmail.com>
2019-01-19 23:54:06 +0700

    done

--- Queue.js
+++ Queue.js
@@ -23,6 +23,9 @@ class Queue {
     this.data[this.tail] = x;
     this.size++;
     this.tail++;
+    if(this.data.tail == this.max){
+      this.tail=0;
+    }
     return true;
   }
 // output:  ถ้า list มีข้อมูลจะ return ข้อมูล , ถ้า list ว่างจะ return null
@@ -33,8 +36,9 @@ class Queue {
     const x = this.data[this.head];
     this.size--;
     this.head++;
+    this.tail--;
     if(this.head == this.max) {
-      this.head==0;
+      this.head = 0;
     }
     return x;
   }
